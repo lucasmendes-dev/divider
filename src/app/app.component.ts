8ee1a5d9a15ef92854167f7f73dfe690595bf989
Lucas Mendes <lucas.a.mendes97@hotmail.com>
2024-04-25 20:57:57 -0300

    first steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Person } from './Interfaces/Person';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'divider';
+
+  newPersonName: string = '';
+  people: Person[] = [];
+  total:number = 0;
+
+  newItem: string = '';
+  Itens: Person[] = [];
+
+  addPerson() {
+    if (this.newPersonName !== '') {
+      const newPerson: Person = {
+        name: this.newPersonName,
+        cost: 0
+      }
+      this.people.push(newPerson);
+      this.newPersonName = ''
+    }
+  }
+
+  removePerson(index: number) {
+    this.people.splice(index, 1);
+  }
+
 }
---
+++ src/app/person-modal/person-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-modal',
+  templateUrl: './person-modal.component.html',
+  styleUrls: ['./person-modal.component.css']
+})
+export class PersonModalComponent {
+
+}
